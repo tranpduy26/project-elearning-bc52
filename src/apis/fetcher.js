3c73cd6507e09b7a6f0ef04a13e65031614d842b
DuyTran <trandb26@gmail.com>
2023-10-26 16:52:11 +0700

    up

--- src/apis/fetcher.js
+++ src/apis/fetcher.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Setup axios instance
 const fetcher = axios.create({
-  baseURL: "https://movienew.cybersoft.edu.vn/api",
+  baseURL: "https://elearningnew.cybersoft.edu.vn/api",
   headers: {
     TokenCybersoft:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1MiIsIkhldEhhblN0cmluZyI6IjI4LzAyLzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcwOTA3ODQwMDAwMCIsIm5iZiI6MTY4MTE0NjAwMCwiZXhwIjoxNzA5MjI2MDAwfQ.GboZ7OZlrOvJ_T6lEZ9PfGJD8vygDn30BxaLgB43WbM",
---
+++ src/apis/userAPI.js
@@ -0,0 +1,21 @@
+import fetcher from "./fetcher";
+
+// apiSignin
+export const signin = async (payload) => {
+  try {
+    const response = await fetcher.post("/QuanLyNguoiDung/DangNhap", payload);
+    return response.data?.content;
+  } catch (error) {
+    throw error.response.data?.content;
+  }
+};
+
+// apiSignup
+export const signup = async (payload) => {
+  try {
+    const response = await fetcher.post("/QuanLyNguoiDung/DangKy", payload);
+    return response.data?.content;
+  } catch (error) {
+    throw error.response.data?.content;
+  }
+};
